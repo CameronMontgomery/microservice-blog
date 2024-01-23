91a3245bc146391ed3afb47e630c7ea5d642173b
Cameron Montgomery <66389466+CameronMontgomery@users.noreply.github.com>
2024-01-23 12:52:26 -0700

    Update URL to ClusterIP URL for event-bus and posts

--- event-bus/index.mjs
+++ event-bus/index.mjs
@@ -12,18 +12,18 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
     console.log(err.message, 'Posts');
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
-    console.log(err.message, 'Comments');
-  });
-  axios.post('http://localhost:4002/events', event).catch((err) => {
-    console.log(err.message, 'Query');
-  });
-  axios.post('http://localhost:4003/events', event).catch((err) => {
-    console.log(err.message, 'Moderation');
-  });
+  // axios.post('http://localhost:4001/events', event).catch((err) => {
+  //   console.log(err.message, 'Comments');
+  // });
+  // axios.post('http://localhost:4002/events', event).catch((err) => {
+  //   console.log(err.message, 'Query');
+  // });
+  // axios.post('http://localhost:4003/events', event).catch((err) => {
+  //   console.log(err.message, 'Moderation');
+  // });
 
   res.send({ status: 'ok' });
 });
